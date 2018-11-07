cff33a48a7dd062569218502aa2faa0a670d9951
Kerim Gueney <kerim.gueney@taskforce-it.de>
2018-11-07 13:41:11 +0100

    rtrack and display user agents

--- server/index.js
+++ server/index.js
@@ -1,34 +1,57 @@
 const fs = require("fs");
 const app = require("express")();
-const server = require("https").createServer(
-  {
-    key: fs.readFileSync(
-      "/etc/letsencrypt/live/p3.taskforce-it.de/privkey.pem"
-    ),
-    cert: fs.readFileSync(
-      "/etc/letsencrypt/live/p3.taskforce-it.de/fullchain.pem"
-    )
-  },
-  app
-);
+const https = require("https");
+const http = require("http");
+let server;
+if (fs.existsSync("/etc/letsencrypt/live/p3.taskforce-it.de/privkey.pem")) {
+  server = https.createServer(
+    {
+      key: fs.readFileSync(
+        "/etc/letsencrypt/live/p3.taskforce-it.de/privkey.pem"
+      ),
+      cert: fs.readFileSync(
+        "/etc/letsencrypt/live/p3.taskforce-it.de/fullchain.pem"
+      )
+    },
+    app
+  );
+} else {
+  server = http.createServer(app);
+}
+
 const io = require("socket.io")(server);
 const generateId = require("./idGenerator");
 const path = require("path");
 const payload = require("./payload.json");
 
 let clients = [];
-let userAgents = [];
+let userAgents = {
+  Android: 0,
+  iOS: 0,
+  Windows: 0,
+  Unbekannt: 0
+};
 let randomClient;
 
 io.on("connection", client => {
+  let userAgent = client.handshake.headers["user-agent"];
   if (!client.handshake.query.admin) {
     client.customId = generateRandomId();
     clients.push(client);
-    console.log(`client: ${client.customId} connected`);
     client.emit("clientId", client.customId);
+    if (userAgent.match(/windows/gi)) {
+      userAgents.Windows++;
+    } else if (userAgent.match(/android/gi)) {
+      userAgents.Android++;
+    } else if (userAgent.match(/mac os/gi)) {
+      userAgents.iOS++;
+    } else {
+      userAgents.Unbekannt++;
+    }
   }
 
   io.sockets.emit("users", clients.length);
+  io.sockets.emit("user-agents", userAgents);
 
   client.on("admin", data => {
     randomClient = clients[Math.floor(Math.random() * clients.length)];
@@ -49,10 +72,22 @@ io.on("connection", client => {
     console.log(`client: ${client.customId} disconnected`);
     let index = clients.filter((element, index) => {
       if (client.id === element.id) {
+        let thisUserAgent = element.handshake.headers["user-agent"];
+
+        if (thisUserAgent.match(/windows/gi)) {
+          userAgents.Windows--;
+        } else if (thisUserAgent.match(/android/gi)) {
+          userAgents.Android--;
+        } else if (thisUserAgent.match(/mac os/gi)) {
+          userAgents.iOS--;
+        } else {
+          userAgents.Unbekannt--;
+        }
         return index;
       }
     });
     clients.splice(index, 1);
+    io.sockets.emit("user-agents", userAgents);
     io.sockets.emit("users", clients.length);
   });
 });
